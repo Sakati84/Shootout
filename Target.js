4f3a339accca7103a41dfb055ae3b796ec0b5156
sakati <sakati84@googlemail.com>
2026-03-11 21:59:37 +0100

    anim

--- Target.js
+++ Target.js
@@ -1,7 +1,11 @@
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.127/examples/jsm/loaders/GLTFLoader.js';
-//import * as TWEEN from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/16.7.0/Tween.min.js';
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.127/build/three.module.js';
 import TWEEN from 'https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.5.0/dist/tween.esm.js';
+
+const DOWN_ANGLE = -Math.PI / 2;
+const UP_ANGLE = 0;
+const FLIP_DURATION_MS = 350;
+const HOLD_DURATION_MS = 2000;
 
 export default class Target 
 {
@@ -9,59 +13,88 @@ export default class Target
     {
         this.x = x;
         this.y = y;
+        this.pivot = null;
+        this.model = null;
 
-        this.loadModel('mesh/enemy1.gltf', x, y, scene)
+        this.ready = this.loadModel('mesh/enemy1.gltf', x, y, scene);
     }
 
     loadModel(path, x, y, scene)
     {
         const loader = new GLTFLoader();
 
-        loader.load(path, function ( gltf ) 
-        {
-            gltf.scene.rotation.y = -Math.PI / 2;
-            gltf.scene.position.x = x;
-            gltf.scene.position.y = y;
-            
-            //animation test
-            var p = new THREE.Vector3(10, 0, 0); // create on init
-            /*animateVector3(gltf.scene.position, p, {
-    
-                duration: 5000, 
-                
-                easing : TWEEN.Easing.Quadratic.InOut,
-                
-                update: function(d) {
-                },
-                
-                callback : function(){
-                }});*/
-            
-            scene.add( gltf.scene );
-        }, undefined, function ( error ) 
-        {
-            console.error( error );
-        } );
+        return new Promise((resolve, reject) => {
+            loader.load(path, (gltf) => 
+            {
+                const model = gltf.scene;
+                model.rotation.y = -Math.PI / 2;
+
+                const box = new THREE.Box3().setFromObject(model);
+                const center = box.getCenter(new THREE.Vector3());
+                const min = box.min.clone();
+
+                model.position.x -= center.x;
+                model.position.y -= min.y;
+                model.position.z -= center.z;
+
+                const pivot = new THREE.Group();
+                pivot.position.set(x, y, 0);
+                pivot.rotation.x = DOWN_ANGLE;
+                pivot.add(model);
+
+                scene.add(pivot);
+
+                this.pivot = pivot;
+                this.model = model;
+
+                resolve(this);
+            }, undefined, (error) => 
+            {
+                console.error(error);
+                reject(error);
+            });
+        });
     } 
-}
 
-function animateVector3(vectorToAnimate, target, options)
-    {    
-        options = options || {};    // get targets from options or set to defaults
-        var to = target || THREE.Vector3(),
-            easing = options.easing || TWEEN.Easing.Quadratic.In,
-            duration = options.duration || 2000;    // create the tween
-        var tweenVector3 = new TWEEN.Tween(vectorToAnimate)
-            .to({ x: to.x, y: to.y, z: to.z, }, duration)
-            .easing(easing)
-            .onUpdate(function(d) {
-                if(options.update) options.update(d);
-            })
-            .onComplete(function(){
-                if(options.callback) options.callback();
-            });    // start the tween
-
-        tweenVector3.start();    // return the tween in case we want to manipulate it later on
-
-        return tweenVector3;
+    raise()
+    {
+        return this.animateTo(UP_ANGLE);
     }
+
+    lower()
+    {
+        return this.animateTo(DOWN_ANGLE);
+    }
+
+    animateTo(rotationX)
+    {
+        return new Promise((resolve) => {
+            new TWEEN.Tween(this.pivot.rotation)
+                .to({ x: rotationX }, FLIP_DURATION_MS)
+                .easing(TWEEN.Easing.Quadratic.InOut)
+                .onComplete(resolve)
+                .start();
+        });
+    }
+
+    static wait(duration)
+    {
+        return new Promise((resolve) => {
+            window.setTimeout(resolve, duration);
+        });
+    }
+
+    static async runCycle(targets)
+    {
+        while (true)
+        {
+            for (const target of targets)
+            {
+                await target.raise();
+                await Target.wait(HOLD_DURATION_MS);
+                await target.lower();
+                await Target.wait(250);
+            }
+        }
+    }
+}
